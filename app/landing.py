7e808aa33d5208a28bbfa8521e9b965ed1168eb4
KremerMichael <kremeremichael@gmail.com>
2022-11-05 16:58:38 -0700

    messing with landing

--- app/landing.py
+++ app/landing.py
@@ -4,7 +4,7 @@ from queries import (
     get_follower_set,
 )
 
-st.title("touchgrass")
+st.title("🌱 touchgrass 🌿")
 
 alex_address = "0xBE5F037E9bDfeA1E5c3c815Eb4aDeB1D9AB0137B"
 stani_address = "0x7241DDDec3A6aF367882eAF9651b87E1C7549Dff"
@@ -20,32 +20,43 @@ handle = st.selectbox(
 selected_profile = profiles.loc[profiles['handle'] == handle]
 
 # TODO: pretty print all of this
-
-st.write(selected_profile)
-
 image_url = selected_profile['original_url'].values[0]
 
+# get data from profile
+name = selected_profile['name'].values[0]
+bio = selected_profile['bio'].values[0]
+totalFollowers = selected_profile['totalFollowers'].values[0]
+totalFollowing = selected_profile['totalFollowing'].values[0]
+totalPosts = selected_profile['totalPosts'].values[0]
+totalComments = selected_profile['totalComments'].values[0]
+totalMirrors = selected_profile['totalMirrors'].values[0]
+totalPublications = selected_profile['totalPublications'].values[0]
+totalCollects = selected_profile['totalCollects'].values[0]
+
+
+# ***** APP Visuals *****
+col1, col2 = st.columns(2)
+
+# profile image, default to empty
+image_holder = col1.empty()
+image_holder.image('https://via.placeholder.com/150')
+
+# Check for profile image
 if image_url is not None:
     if 'ipfs' in image_url and 'https' not in image_url:
         ipfs_object = image_url.split('/')[-1]
         image_url = f'https://lens.infura-ipfs.io/ipfs/{ipfs_object}'
+    image_holder.image(image_url)
 
-    st.image(image_url)
+# profile info
+col2.write(f'**{name}**: {bio}')
+col2.write(f'Followers: {totalFollowers} Following: {totalFollowing}')
+col2.write(f'Posts: {totalPosts} Comments: {totalComments}')
+col2.write(f'Mirrors: {totalMirrors} Publications: {totalPublications} Collects: {totalCollects}')
 
-for col in [
-    'totalFollowers',
-    'totalFollowing',
-    'totalPosts',
-    'totalComments',
-    'totalMirrors',
-    'totalPublications',
-    'totalCollects',
-]:
-    st.write(f"{col}: {selected_profile[col].values[0]}")
-
+# profile followers
 st.write("Follower set")
 selected_profile_id = selected_profile['id'].values[0]
 
 follower_set = get_follower_set(selected_profile_id)
 st.dataframe(follower_set)
-
